9a2985585a0272d13dc14ba45d63a17f1d6f5f39
Santiago Arellano <santiagoarellano703@gmail.com>
2025-11-23 00:27:47 -0400

    fix: configure FastAPI with custom docs and redoc URLs

--- src/app.py
+++ src/app.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
 from src.adapters.api.routers import download_route
 
-app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 app.include_router(download_route.router)
+
+
+@app.get("/")
+async def root():
+    return Response(status_code=204)
