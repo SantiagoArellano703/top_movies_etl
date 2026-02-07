882fe04720618e1e05b36cc753ef146d0e35ecab
Santiago Arellano <santiagoarellano703@gmail.com>
2026-02-07 00:00:44 -0400

    feat: refactor download use case and transformers to utilize Platform value object

--- src/adapters/factory/scraper_factory.py
+++ src/adapters/factory/scraper_factory.py
@@ -1,5 +1,6 @@
 import logging
 from src.core.domain.ports.scraper_port import ScraperPort
+from src.core.domain.value_objects.platform import Platform
 from src.adapters.scrapers.tmdb_scraper import TMDBScraper
 
 logger = logging.getLogger(__name__)
@@ -7,14 +8,13 @@ logger = logging.getLogger(__name__)
 
 class ScraperFactory:
     scrapers = {
-        "tmdb": TMDBScraper,
-        # "rottentomatoes": RottenTomatoesScraper
+        Platform.TMDB: TMDBScraper,
     }
 
     @staticmethod
-    def get_scraper(platform: str) -> ScraperPort:
-        scraper_class = ScraperFactory.scrapers.get(platform.lower())
+    def get_scraper(platform: Platform) -> ScraperPort:
+        scraper_class = ScraperFactory.scrapers.get(platform)
         if not scraper_class:
-            raise ValueError(f"Unknown platform: {platform}")
+            raise ValueError(f"Unknown scraper for: {platform.key}")
         logger.info(f"Init Scraper {scraper_class.__name__}")
         return scraper_class()
